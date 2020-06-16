1f9603531906de02e57fd18f62fc762cf2ce5d25
github username <github email address>
2020-06-15 22:11:14 -0500

    sign-in, sign-up, sign-out pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  include Clearance::User
+
   has_many :matches
   has_many :dogs, through: :matches
   has_many :reviews, through: :matches
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_013845) do
+ActiveRecord::Schema.define(version: 2020_06_16_025626) do
 
   create_table "dogs", force: :cascade do |t|
     t.string "name"
@@ -48,6 +48,12 @@ ActiveRecord::Schema.define(version: 2020_06_16_013845) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
+    t.string "encrypted_password", limit: 128
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
 end
